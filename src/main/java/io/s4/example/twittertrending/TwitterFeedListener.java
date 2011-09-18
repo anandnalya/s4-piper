546490015f0212960ea2e88cb0f35c0491d526e1
Anand Nalya <anand.nalya@gmail.com>
2011-09-18 11:19:53 +0530

    Removed log4j logger

--- src/main/java/io/s4/example/twittertrending/TwitterFeedListener.java
+++ src/main/java/io/s4/example/twittertrending/TwitterFeedListener.java
@@ -11,7 +11,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.util.EncodingUtil;
-import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,132 +25,126 @@ public class TwitterFeedListener {
     private LinkedBlockingQueue<String> messageQueue = new LinkedBlockingQueue<String>();
 
     public TwitterFeedListener(Stream<StatusEvent> statusStream) {
-	this.statusStream = statusStream;
+        this.statusStream = statusStream;
     }
 
     public void init() {
-	(new Thread(new Dequeuer())).start();
+        (new Thread(new Dequeuer())).start();
     }
 
     public void setUserid(String userid) {
-	this.userid = userid;
+        this.userid = userid;
     }
 
     public void setPassword(String password) {
-	this.password = password;
+        this.password = password;
     }
 
     public void setUrlString(String urlString) {
-	this.urlString = urlString;
+        this.urlString = urlString;
     }
 
     public void setMaxBackoffTime(long maxBackoffTime) {
-	this.maxBackoffTime = maxBackoffTime;
+        this.maxBackoffTime = maxBackoffTime;
     }
 
     public void run() {
-	long backoffTime = 1000;
-	while (!Thread.interrupted()) {
-	    try {
-		connectAndRead();
-	    } catch (Exception e) {
-		Logger.getLogger("s4").error("Exception reading feed", e);
-		try {
-		    Thread.sleep(backoffTime);
-		} catch (InterruptedException ie) {
-		    Thread.currentThread().interrupt();
-		}
-		backoffTime = backoffTime * 2;
-		if (backoffTime > maxBackoffTime) {
-		    backoffTime = maxBackoffTime;
-		}
-	    }
-	}
+        long backoffTime = 1000;
+        while (!Thread.interrupted()) {
+            try {
+                connectAndRead();
+            } catch (Exception e) {
+                try {
+                    Thread.sleep(backoffTime);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                }
+                backoffTime = backoffTime * 2;
+                if (backoffTime > maxBackoffTime) {
+                    backoffTime = maxBackoffTime;
+                }
+            }
+        }
     }
 
     public void connectAndRead() throws Exception {
-	URL url = new URL(urlString);
+        URL url = new URL(urlString);
 
-	URLConnection connection = url.openConnection();
-	String userPassword = userid + ":" + password;
-	String encoded = EncodingUtil.getAsciiString(Base64
-		.encodeBase64(EncodingUtil.getAsciiBytes(userPassword)));
-	connection.setRequestProperty("Authorization", "Basic " + encoded);
-	connection.connect();
-	System.out.println("connected");
+        URLConnection connection = url.openConnection();
+        String userPassword = userid + ":" + password;
+        String encoded = EncodingUtil.getAsciiString(Base64
+                .encodeBase64(EncodingUtil.getAsciiBytes(userPassword)));
+        connection.setRequestProperty("Authorization", "Basic " + encoded);
+        connection.connect();
+        System.out.println("connected");
 
-	InputStream is = connection.getInputStream();
-	InputStreamReader isr = new InputStreamReader(is);
-	BufferedReader br = new BufferedReader(isr);
+        InputStream is = connection.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
 
-	String inputLine = null;
-	while ((inputLine = br.readLine()) != null) {
-//	    System.out.println(inputLine);
-	    if (inputLine.trim().length() == 0) {
-		blankCount++;
-		continue;
-	    }
-	    messageCount++;
-	    
-	    messageQueue.add(inputLine);
-	}
+        String inputLine = null;
+        while ((inputLine = br.readLine()) != null) {
+            System.out.println(inputLine);
+            if (inputLine.trim().length() == 0) {
+                blankCount++;
+                continue;
+            }
+            messageCount++;
+
+            messageQueue.add(inputLine);
+        }
     }
 
     class Dequeuer implements Runnable {
-	public void run() {
-	    while (!Thread.interrupted()) {
-		try {
-		    String message = messageQueue.take();
-		    JSONObject messageJSON = new JSONObject(message);
+        public void run() {
+            while (!Thread.interrupted()) {
+                try {
+                    String message = messageQueue.take();
+                    JSONObject messageJSON = new JSONObject(message);
 
-		    // ignore delete records for now
-		    if (messageJSON.has("delete")) {
-			continue;
-		    }
+                    // ignore delete records for now
+                    if (messageJSON.has("delete")) {
+                        continue;
+                    }
 
-		    // create a copy with some renamed fields
-		    StatusEvent event = parseStatus(messageJSON);
-		    statusStream.put(event);
-		} catch (InterruptedException ie) {
-		    Thread.currentThread().interrupt();
-		} catch (JSONException je) {
-		    je.printStackTrace();
-		}
-	    }
-	}
+                    // create a copy with some renamed fields
+                    StatusEvent event = parseStatus(messageJSON);
+                    statusStream.put(event);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                } catch (JSONException je) {
+                    je.printStackTrace();
+                }
+            }
+        }
 
-	public StatusEvent parseStatus(JSONObject origStatusJSON) {
-	    try {
-		if (origStatusJSON == null
-			|| origStatusJSON.equals(JSONObject.NULL)) {
-		    return null;
-		}
+        public StatusEvent parseStatus(JSONObject jsonObj) {
+            if (jsonObj == null || jsonObj.equals(JSONObject.NULL)) {
+                return null;
+            }
 
-		return copyFields(origStatusJSON);
-	    } catch (Exception e) {
-		Logger.getLogger("s4").error(e);
-	    }
+            try {
+                StatusEvent event = new StatusEvent();
+                event.setCreatedAt(jsonObj.getString("created_at"));
+                event.setId(jsonObj.getLong("id"));
+                event.setSource(jsonObj.getString("source"));
+                event.setText(jsonObj.getString("text"));
 
-	    return null;
-	}
+                JSONObject userJson = jsonObj.getJSONObject("user");
+                User user = new User();
+                user.setId(userJson.getLong("id"));
+                user.setLocation(userJson.getString("location"));
+                user.setName(userJson.getString("screen_name"));
 
-	public StatusEvent copyFields(JSONObject from) throws JSONException {
+                event.setUser(user);
 
-	    StatusEvent event = new StatusEvent();
-	    event.setCreatedAt(from.getString("created_at"));
-	    event.setId(from.getLong("id"));
-	    event.setSource(from.getString("source"));
-	    event.setText(from.getString("text"));
+                return event;
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-	    JSONObject userJson = from.getJSONObject("user");
-	    User user = new User();
-	    user.setId(userJson.getLong("id"));
-	    user.setLocation(userJson.getString("location"));
-	    user.setName(userJson.getString("screen_name"));
-
-	    event.setUser(user);
-
-	    return event;
-	}
+            return null;
+        }
     }
 }
